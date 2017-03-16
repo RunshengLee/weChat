dbe9b66e770ceeac4bb144832565b0478698a9d7
runshenglee <494671597@qq.com>
2017-03-16 16:53:24 +0800

    go

--- python/FlaskweChat.py
+++ python/FlaskweChat.py
@@ -1,23 +1,37 @@
-import hashlib
-from flask import Flask, request, render_template, jsonify, make_response
+# -*- coding=utf-8 -*-
 import time
-import _elementtree as ET
+from flask import Flask,g,request,make_response
+import hashlib
+import xml.etree.ElementTree as ET
 
 app = Flask(__name__)
-app.debug = True
+app.debug=True
 
-
-@app.route('/root/weChat/python/', methods=['GET', 'POST'])
+@app.route('/root/weChat/python/',methods=['GET','POST'])
 def wechat_auth():
     if request.method == 'GET':
-        token = 'weixin'  # your token
-    query = request.args
-    signature = query.get('signature', '')
-    timestamp = query.get('timestamp', '')
-    nonce = query.get('nonce', '')
-    echostr = query.get('echostr', '')
-    s = [timestamp, nonce, token]
-    s.sort()
-    s = ''.join(s)
-    if hashlib.sha1(s).hexdigest() == signature:
-        return make_response(echostr)  
+        token='weixin' #微信配置所需的token
+        data = request.args
+        signature = data.get('signature','')
+        timestamp = data.get('timestamp','')
+        nonce = data.get('nonce','')
+        echostr = data.get('echostr','')
+        s = [timestamp,nonce,token]
+        s.sort()
+        s = ''.join(s)
+        if (hashlib.sha1(s).hexdigest() == signature):
+            return make_response(echostr)
+    else:
+        rec = request.stream.read()
+        xml_rec = ET.fromstring(rec)
+        tou = xml_rec.find('ToUserName').text
+        fromu = xml_rec.find('FromUserName').text
+        content = xml_rec.find('Content').text
+        xml_rep = "<xml><ToUserName><![CDATA[%s]]></ToUserName><FromUserName><![CDATA[%s]]></FromUserName><CreateTime>%s</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[%s]]></Content><FuncFlag>0</FuncFlag></xml>"
+        response = make_response(xml_rep % (fromu,tou,str(int(time.time())), content))
+        response.content_type='application/xml'
+        return response
+    return 'Hello weixin!'
+
+if __name__ == '__main__':
+    app.run()
