2edbd535b5ae84fc0f2a778c8cb0ff97917047c7
runshenglee <494671597@qq.com>
2017-03-16 15:16:48 +0800

    go

--- python/FlaskweChat.py
+++ python/FlaskweChat.py
@@ -1,12 +1,23 @@
-from flask import Flask
+import hashlib
+from flask import Flask, request, render_template, jsonify, make_response
+import time
+import _elementtree as ET
 
 app = Flask(__name__)
+app.debug = True
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-
-if __name__ == '__main__':
-    app.run()
+@app.route('/weixin/', methods=['GET', 'POST'])
+def wechat_auth():
+    if request.method == 'GET':
+        token = '****'  # your token
+    query = request.args
+    signature = query.get('signature', '')
+    timestamp = query.get('timestamp', '')
+    nonce = query.get('nonce', '')
+    echostr = query.get('echostr', '')
+    s = [timestamp, nonce, token]
+    s.sort()
+    s = ''.join(s)
+    if hashlib.sha1(s).hexdigest() == signature:
+        return make_response(echostr)  
